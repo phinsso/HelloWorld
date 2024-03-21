45f3054fda5190cb306da1e4f13f64910340b699
phinsso <naso01247@gmail.com>
2024-03-21 10:54:56 +0900

    초기 커밋.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld // namespace: 자바에서 패키지와 같은 개념
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
